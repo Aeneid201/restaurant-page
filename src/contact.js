8bd471e5c90100c6e5d7cffe6ce99d3e3db1af91
Aeneid201 <aloubi.oumaima@gmail.com>
2022-11-04 22:11:42 -0400

    Added pages and fonts

--- src/contact.js
+++ src/contact.js
@@ -1,5 +1,24 @@
 function contactPage() {
   let content = document.querySelector("#content");
+  let html = `<div class="home bg">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 col-sm-12">
+        <div class="inner">
+        <h3>Contact Info</h3>
+        <ul>
+        <li>Phone Number: 514-555-5555</li>
+        <li>Email: info@resto.ca</li>
+        <li>Address: 123 Fake Street, Montreal, QC</li>
+        </ul>
+        </div>
+
+      
+        </div>
+      </div>
+    </div>
+  </div>`;
+  content.insertAdjacentHTML("afterbegin", html);
 }
 
 export { contactPage };
